1cc48538d31059b28538f13e11d8545c02657c8a
Chris Peralta <chrisperalta1998@gmail.com>
2022-12-28 19:52:57 -0800

    finished user req

--- backend_PELCM/controllers/UserController.js
+++ backend_PELCM/controllers/UserController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 const getUser = async (req, res, next) => {
     const id = req.params.id;
@@ -20,20 +21,109 @@ const updateUser = async (req, res, next) => {
     const id = req.params.id;
     const { currentUserId, currentUserAdminStatus, password } = req.body;
 
-    if (id === currentUserId) {
-    }
+    if (id === currentUserId || currentUserAdminStatus) {
+        try {
+            if (password) {
+                req.body.password = bcrypt.hashSync(password);
+            }
 
-    // try {
-    //     const user = await User.findById(id);
-    //     if (user) {
-    //         const { password, ...otherDetails } = user._doc;
-    //         res.status(200).json(otherDetails);
-    //     } else {
-    //         res.status(400).json({ message: "user does not exist" });
-    //     }
-    // } catch (err) {
-    //     res.status(500).json(err);
-    // }
+            const user = await User.findByIdAndUpdate(id, req.body, {
+                // gives me the updated user not prev user
+                new: true,
+            });
+
+            res.status(200).json(user);
+        } catch (error) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json({
+            message:
+                "You don't have the power for that. You can only change your own profile!",
+        });
+    }
+};
+
+const deleteUser = async (req, res, next) => {
+    const id = req.params.id;
+    const { currentUserId, currentUserAdminStatus } = req.body;
+
+    if (id === currentUserId || currentUserAdminStatus) {
+        try {
+            await User.findByIdAndDelete(id);
+
+            res.status(200).json({ message: "User deleted successfully." });
+        } catch (error) {
+            res.status(500).json(err);
+        }
+    } else {
+        res.status(403).json({
+            message:
+                "You don't have the power for that. You can only delete your own profile!",
+        });
+    }
+};
+
+const followUser = async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId } = req.body;
+
+    if (currentUserId === id) {
+        res.status(403).json({ message: "Action forbidden" });
+    } else {
+        try {
+            const followUser = await User.findById(id);
+            const followingUser = await User.findById(currentUserId);
+
+            if (!followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({
+                    $push: { followers: currentUserId },
+                });
+                await followingUser.updateOne({
+                    $push: { followings: id },
+                });
+
+                res.status(200).json({ message: "User followed!" });
+            } else {
+                res.status(403).json({
+                    message: "User is already followed by you",
+                });
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
+};
+
+const unfollowUser = async (req, res) => {
+    const id = req.params.id;
+    const { currentUserId } = req.body;
+
+    if (currentUserId === id) {
+        res.status(403).json({ message: "Action forbidden" });
+    } else {
+        try {
+            const followUser = await User.findById(id);
+            const followingUser = await User.findById(currentUserId);
+
+            if (followUser.followers.includes(currentUserId)) {
+                await followUser.updateOne({
+                    $pull: { followers: currentUserId },
+                });
+                await followingUser.updateOne({
+                    $pull: { followings: id },
+                });
+
+                res.status(200).json({ message: "User unfollowed!" });
+            } else {
+                res.status(403).json({
+                    message: "User is not followed by you",
+                });
+            }
+        } catch (error) {
+            res.status(500).json(error);
+        }
+    }
 };
 
 const getAllUser = async (req, res, next) => {
@@ -52,4 +142,11 @@ const getAllUser = async (req, res, next) => {
     return res.status(200).json({ users });
 };
 
-module.exports = { getAllUser, getUser, updateUser };
+module.exports = {
+    getAllUser,
+    getUser,
+    updateUser,
+    deleteUser,
+    followUser,
+    unfollowUser,
+};
--- backend_PELCM/routes/UserRoute.js
+++ backend_PELCM/routes/UserRoute.js
@@ -1,9 +1,20 @@
 const express = require("express");
-const { getUser, getAllUser } = require("../controllers/UserController");
+const {
+    getUser,
+    getAllUser,
+    updateUser,
+    deleteUser,
+    followUser,
+    unfollowUser,
+} = require("../controllers/UserController");
 
 const router = express.Router();
 
 router.get("/:id", getUser);
+router.put("/:id", updateUser);
+router.delete("/:id", deleteUser);
+router.put("/:id/follow", followUser);
+router.put("/:id/unfollow", unfollowUser);
 router.get("/users", getAllUser);
 
 module.exports = router;
